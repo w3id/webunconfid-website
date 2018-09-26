673000791bff79d6b3860f691145c7988c3d0a5c
Yohan Totting <tyohan@gmail.com>
2018-09-26 22:58:15 +0700

    re-enable SW

--- src/t-app.js
+++ src/t-app.js
@@ -32,29 +32,29 @@ export class TApp extends HTMLElement {
     connectedCallback(){
         this.render();
         this.scrollTo(this.getAttribute('active'));
-        // register('/service-worker.js', {
-        //     ready (registration) {
-        //     console.log('Service worker is active.')
-        //     },
-        //     registered (registration) {
-        //     console.log('Service worker has been registered.')
-        //     },
-        //     cached (registration) {
-        //     console.log('Content has been cached for offline use.')
-        //     },
-        //     updatefound (registration) {
-        //     console.log('New content is downloading.')
-        //     },
-        //     updated (registration) {
-        //     console.log('New content is available; please refresh.')
-        //     },
-        //     offline () {
-        //     console.log('No internet connection found. App is running in offline mode.')
-        //     },
-        //     error (error) {
-        //     console.error('Error during service worker registration:', error)
-        //     }
-        // });
+        register('/service-worker.js', {
+            ready (registration) {
+            console.log('Service worker is active.')
+            },
+            registered (registration) {
+            console.log('Service worker has been registered.')
+            },
+            cached (registration) {
+            console.log('Content has been cached for offline use.')
+            },
+            updatefound (registration) {
+            console.log('New content is downloading.')
+            },
+            updated (registration) {
+            console.log('New content is available; please refresh.')
+            },
+            offline () {
+            console.log('No internet connection found. App is running in offline mode.')
+            },
+            error (error) {
+            console.error('Error during service worker registration:', error)
+            }
+        });
     }
 
     get template(){
