b91247492f9b59ba92010100eeb698502ae7e906
Satya Kresna <satyakresna6295@gmail.com>
2018-09-27 14:05:47 +0800

    Attach t-communities custom elements

--- src/t-app.js
+++ src/t-app.js
@@ -224,33 +224,7 @@ export class TApp extends HTMLElement {
                     </div>
                 </div>
                 <div id="community-content" class="block-content">
-                <div class="block-content" id="community">
-                    <h2>Komunitas Yang Hadir</h2>
-                    <div>
-                        <ul id="community-list">
-                            <li><a href="//t.me/femalegeek" target="_blank" rel="noopener">Female Geeks</a></li>
-                            <li><a href="//t.me/react_id" target="_blank" rel="noopener">ReactID</a></li>
-                            <li><a href="//t.me/vuejsid" target="_blank" rel="noopener">VueID</a></li>
-                            <li><a href="//t.me/AngularID" target="_blank" rel="noopener">AngularID</a></li>
-                            <li><a href="//www.phpindonesia.id/" target="_blank" rel="noopener">PHP Indonesia</a></li>
-                            <li><a href="//t.me/laravelindonesia" target="_blank" rel="noopener">Laravel Indonesia</a></li>
-                            <li><a href="https://t.me/YiiFrameworkIndonesia" target="_blank" rel="noopener">Yii Indonesia</a></li>
-                            <li><a href="http://www.generationgirl.org/" target="_blank" rel="noopener">Generation Girl</a></li>
-                            <li><a href="//wp-id.org/" target="_blank" rel="noopener">WordPress Indonesia</a></li>
-                            <li><a href="//t.me/polymer_id" target="_blank" rel="noopener">Polymer Indonesia</a></li>
-                            <li><a href="//developers.facebook.com/developercircles" target="_blank" rel="noopener">Facebook Dev Circle</a></li>
-                            <li><a href="//www.meetup.com/JakartaJS/" target="_blank" rel="noopener">JakartaJS</a></li>
-                            <li><a href="//t.me/ruby_id" target="_blank" rel="noopener">RubyID</a></li>
-                            <li><a href="//jogja.js.org/" target="_blank" rel="noopener">JogjaJS</a></li>
-                            <li><a href="//balijs.com" target="_blank" rel="noopener">BaliJS</a></li>
-                            <li><a href="//bit.ly/kpmchat" target="_blank" rel="noopener">Komunitas Programmer Makassar</a></li>
-                            <li><a href="//www.facebook.com/groups/frontendev.malang" target="_blank" rel="noopener">Malang Frontend Dev</a></li>
-                            <li><a href="//www.meetup.com/BandungJS/" target="_blank" rel="noopener">BandungJS</a></li>
-                            <li><a href="//t.me/surabayadev" target="_blank" rel="noopener">SurabayaDev</a></li>
-                            <li><a href="//hammer-code.github.io" target="_blank" rel="noopener">Hammer-Code</a></li>
-                            <li><a href="//phpbali.com" target="_blank" rel="noopener">PHPBali</a></li>
-                        </ul>
-                    </div>
+                    <t-communities></t-communities>
                 </div>
                 <div class="block-content" id="organizer-list">
                     <t-organizers></t-organizers>
